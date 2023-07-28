943b7864353b7ed6395f4134f130c30835c3d24b
swmao <WinsM@pku.edu.cn>
2023-07-28 19:19:17 +0800

    [Mod] cinema content

--- cinemas/content.py
+++ cinemas/content.py
@@ -4,26 +4,40 @@
 """
 import pandas as pd
 import os
-PATH = '/mnt/d/Codes_代码/mygitrep/Travel-Tales-Tickets/cinemas/'
+
+force_update = False
+src = './cinemas/cinemas.xlsx'
+tgt = './cinemas/tickets/content/tickets/'
+
+
+def trans_seat(s):
+    s = s.replace('排', '-')
+    s = s.replace('座', '')
+    s = s.replace('列', '')
+    s = s.replace('一', '1')
+    s = s.replace('二', '2')
+    s = s.replace('层', '楼')
+    s = s.replace(' 1楼', '')
+    s = s.replace(' 2楼', '  2nd')
+    return s
 
 def main():
-    force_update = False
-    src = PATH + 'cinemas.xlsx'
-    tgt = PATH + 'content/'
     os.makedirs(tgt, exist_ok=True)
     df = pd.read_excel(src)
-    df['remark'] = df['remark'].fillna('')
+    # df['remark'] = df['remark'].fillna('')
     for irow in df.iterrows():
         s = irow[1]
         filepath = tgt + f"""{s['movie']}.md"""
         if (not force_update) and os.path.exists(filepath):
             continue
         content = ['---\n']
-        content.append(f"""title: \"{s['movie']}\"\n""")
-        content.append(f"""date: \"{s['date'].strftime('%Y-%m-%d')}\"\n""")
-        content.append(f"""price: \"{s['price']:.2f}\"\n""")
-        content.append(f"""theaters: [\"{s['theaters']}\"]\n""")
-        content.append(f"""remark: {s['remark'].split('；')}\n""")
+        content.append(f"""title: '{s['movie']}'\n""")
+        content.append(f"""date: '{s['date'].strftime('%Y-%m-%d')}'\n""")
+        content.append(f"""price: '{s['price']:.2f}'\n""")
+        content.append(f"""theaters: ['{s['theaters'].replace('（', '·').replace('）','')}']\n""")
+        content.append(f"""seat: ['{trans_seat(s['seat'])}']\n""")
+        if isinstance(s['remark'], str):
+            content.append(f"""remark: {s['remark'].split('；')}\n""")
         content.append('---')
 
         with open(filepath, 'w') as f:
