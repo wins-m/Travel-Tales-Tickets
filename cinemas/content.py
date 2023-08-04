1237bda6573e15e4dd8a117a18d3f892b724ff11
swmao <WinsM@pku.edu.cn>
2023-08-04 12:45:06 +0800

    [Mod] cinema content

--- cinemas/content.py
+++ cinemas/content.py
@@ -2,34 +2,38 @@
 (created by swmao on 28th July)
 
 """
+import argparse
 import pandas as pd
 import os
-
-force_update = False
-src = './cinemas.xlsx'
-tgt = './tickets/content/tickets/'
 
 
 def trans_seat(s):
     s = s.replace('排', '-')
     s = s.replace('座', '')
     s = s.replace('列', '')
+    s = s.replace('号', '')
     s = s.replace('一', '1')
     s = s.replace('二', '2')
     s = s.replace('层', '楼')
-    s = s.replace(' 1楼', '')
-    s = s.replace(' 2楼', '  2nd')
+    s = s.replace('1楼', ' 1F')
+    s = s.replace('2楼', ' 2F')
     return s
 
-def main():
+
+def main(args):
+    force_update = args.force
+    src = f'{args.path}/cinemas.xlsx'
+    tgt = f'{args.path}/tickets/content/tickets/'
     os.makedirs(tgt, exist_ok=True)
     df = pd.read_excel(src)
-    # df['remark'] = df['remark'].fillna('')
+    
+    changed = False 
     for irow in df.iterrows():
         s = irow[1]
         filepath = tgt + f"""{s['movie']}.md"""
         if (not force_update) and os.path.exists(filepath):
             continue
+        changed = True
         content = ['---\n']
         content.append(f"""title: '{s['movie']}'\n""")
         content.append(f"""date: '{s['date'].strftime('%Y-%m-%d')}'\n""")
@@ -44,6 +48,21 @@ def main():
             f.writelines(content)
         print(filepath)
 
+    if changed:
+        tgt_readme = f'{args.path}/README.md'
+        df['date'] = df['date'].apply(lambda x: x.strftime('%Y-%m-%d'))
+        df['time'] = df['time'].apply(lambda x: x.strftime('%H:%M'))
+        df = df.fillna('')
+        with open(tgt_readme, 'w') as f:
+            f.writelines(df.to_markdown(index=False))
+        print(tgt_readme)
+
+
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description="Description of your script")
+    parser.add_argument("path", type=str, help="Specify the path argument")
+    parser.add_argument("--force", action="store_true", help="Specify the force_update argument")
+
+    args = parser.parse_args()
+    main(args)
     
--- cinemas/run.sh
+++ cinemas/run.sh
@@ -1,3 +1,30 @@
-python content.py
-echo "http://localhost:9898/"
-hugo server -s tickets -p 9898 --quiet
+#!/bin/sh
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/winston/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/winston/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/winston/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/winston/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+
+# 获取脚本的名称（包含路径）
+script_name="$0"
+# 使用readlink命令获取脚本的绝对路径
+script_directory=$(dirname "$(readlink -f "$script_name")")
+
+echo "脚本所在目录：$script_directory"
+
+# python "$script_directory/content.py" "$script_directory" --force
+python "$script_directory/content.py" "$script_directory"
+
+echo "网页所在地址：http://localhost:9898/"
+hugo server -s "$script_directory/tickets" -p 9898 --quiet
